a4ec18e16e00b42851967891958667e07b816a49
kuriajames147 <117153308+kuriajames147@users.noreply.github.com>
2025-05-14 11:33:44 +0300

    Update answers.sql

---
+++ answers.sql
@@ -0,0 +1,43 @@
+-- QUESTION 1
+CREATE TABLE ProductDetail_1NF (
+    OrderID INT,
+    CustomerName VARCHAR(100),
+    Product VARCHAR(50)
+);
+
+INSERT INTO ProductDetail_1NF (OrderID, CustomerName, Product)
+VALUES 
+(101, 'John Doe', 'Laptop'),
+(101, 'John Doe', 'Mouse'),
+(102, 'Jane Smith', 'Tablet'),
+(102, 'Jane Smith', 'Keyboard'),
+(102, 'Jane Smith', 'Mouse'),
+(103, 'Emily Clark', 'Phone');
+-- QUESTION 2
+-- CREATE ORDERS TABLE
+CREATE TABLE Orders (
+    OrderID INT PRIMARY KEY,
+    CustomerName VARCHAR(100)
+);
+
+INSERT INTO Orders (OrderID, CustomerName)
+VALUES 
+(101, 'John Doe'),
+(102, 'Jane Smith'),
+(103, 'Emily Clark');
+-- CREATE ORDERDETAILS TABLE
+CREATE TABLE OrderDetails (
+    OrderID INT,
+    Product VARCHAR(50),
+    Quantity INT,
+    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
+);
+
+INSERT INTO OrderDetails (OrderID, Product, Quantity)
+VALUES 
+(101, 'Laptop', 2),
+(101, 'Mouse', 2),
+(102, 'Tablet', 3),
+(102, 'Keyboard', 1),
+(102, 'Mouse', 2),
+(103, 'Phone', 1);
